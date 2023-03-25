6f9bdaadefa0d10a4a6736fc9c3312ce72bb0df2
chavdapradip48 <chavdapradip48@gmail.com>
2023-03-25 12:03:35 +0530

    test

--- room-management-system/src/main/java/com/pradip/roommanagementsystem/RoomManagementSystemApplication.java
+++ room-management-system/src/main/java/com/pradip/roommanagementsystem/RoomManagementSystemApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RoomManagementSystemApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(RoomManagementSystemApplication.class, args);
 	}
 
