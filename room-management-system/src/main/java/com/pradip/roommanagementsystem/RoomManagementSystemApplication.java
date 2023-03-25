63b85dda0cbb83f9bd355b098dc30a007dc280cd
chavdapradip48 <chavdapradip48@gmail.com>
2023-03-25 11:33:08 +0530

    room management project added into git

---
+++ room-management-system/src/main/java/com/pradip/roommanagementsystem/RoomManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pradip.roommanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomManagementSystemApplication.class, args);
+	}
+
+}
