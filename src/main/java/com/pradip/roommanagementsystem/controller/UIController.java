5db0ab4f503a94a69a916714682cf7f91434a2c7
Pradip Chavda <87759455+chavdapradip48@users.noreply.github.com>
2023-04-03 14:57:44 +0530

    keep alive changes

--- src/main/java/com/pradip/roommanagementsystem/controller/UIController.java
+++ src/main/java/com/pradip/roommanagementsystem/controller/UIController.java
@@ -5,10 +5,7 @@ import com.pradip.roommanagementsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,7 @@ public class UIController {
     }
 
     @GetMapping("/keep-alive")
+    @ResponseBody
     public String getKeepAlive(){
         return "Server is running......";
     }
