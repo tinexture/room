cdf37f887a5b9b0431733da9e54c66fa8fa61888
chavdasandip2908 <chavdasandip2908@gmail.com>
2023-04-12 00:09:12 +0530

    api modify and frontend and backend folder created

---
+++ room-frontend-system/asstes/js/forgot.js
@@ -0,0 +1,36 @@
+$("#generate-otp").click(function () {
+    var femailren = emailVlid($('#fEmail').val(), $('#femail-err'));
+    borderErrorColor(femailren, $('#fEmail'));
+    if (femailren) {
+        callSendOtp();
+    }
+});
+$("#verified-otp").click(function () {
+    var otpren = otpVlid($('#otp-field').val(), $('#otp-err'));
+    borderErrorColor(otpren, $('#otp-field'));
+    if (otpren) {
+        $("#verified-otp,#otp-field").prop("disabled", true);
+    }
+});
+
+function callSendOtp() {
+    // $('#cover-spin').show();
+
+    $.ajax({
+        url: apiUrl + "/user/send-otp/" + $('#fEmail').val(),
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (responce) {
+            $(".otp-section").css("display", "block");
+            $("#verified-otp").css("display", "block");
+            $("#generate-otp , #fEmail").prop("disabled", true);
+        },
+        error: function (xhr, status, error) {
+            // var errorMessage = JSON.parse(xhr.responseText);
+            // console.log(errorMessage.message);
+            // $('#cover-spin').hide();
+        }
+    });
+}
---
+++ room-frontend-system/asstes/js/login.js
@@ -0,0 +1,53 @@
+const apiUrl = 'https://roommates-782r.onrender.com';
+$(document).ready(function () {
+    function callLoginApi() {
+        authenticate($("#email").val(), $("#password").val());
+    }
+    function authenticate(email, password) {
+        $('#cover-spin').show();
+        $.ajax({
+            url: apiUrl + "/authenticate",
+            method: "POST",
+            data: JSON.stringify({
+                "email": email,
+                "password": password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            success: function (responce) {
+                console.log("API responce", responce);
+                $('#cover-spin').hide()
+                window.sessionStorage.setItem("token", "Bearer " + responce.jwt);
+                showToast("User logged-in successsfully", 'success');
+                window.location.href = "homePage.html";
+            },
+            error: function (xhr, status, error) {
+                var errorMessage = JSON.parse(xhr.responseText);
+                console.log(errorMessage.message);
+                showToast(errorMessage.message, 'error');
+                $('#cover-spin').hide();
+            }
+        });
+    }
+
+    $('#sub').click(function () {
+        // get value
+        var email = $('#email').val();
+        var pass = $('#password').val();
+        //error element stor 
+        var email_error = $('#email-err');
+        var pass_error = $('#password-err');
+        // call function
+        var email_ret = emailVlid(email, email_error);
+        var pass_ret = passVlid(pass, pass_error);
+        // give error border 
+        borderErrorColor(email_ret, $('#email'));
+        borderErrorColor(pass_ret, $('#password'));
+        // form valide then call "callLoginApi()"
+        if (email_ret && pass_ret) {
+            console.log("form is valid");
+            callLoginApi();
+        }
+    });
+});
